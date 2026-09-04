c6c1c670785c9cdc61548cd961add9fe9b564782
muk1mov <mukimov.shop@gmail.com>
2026-09-04 07:04:40 +0500

    bix fixed ошибки исправленныйправерка запуска

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_PORT = int(os.getenv("DB_PORT", "5432"))
 DB_NAME = os.getenv("DB_NAME", "pdf_books")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS")
--- handlers/qa.py
+++ handlers/qa.py
@@ -1,6 +1,5 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from aiogram.filters import CommandStart
 
 import database as db
 from ai_service import ask_about_book
